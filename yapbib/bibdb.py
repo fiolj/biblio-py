930876ebd59cd7dddf455b8e5a0620838df264ba
Juan Fiol <fiol@cab.cnea.gov.ar>
2022-10-12 16:34:30 -0300

    sigo

--- yapbib/bibdb.py
+++ yapbib/bibdb.py
@@ -3,32 +3,50 @@ import helper
 
 
 class BibDb(object):
-  """Object to manage the connection to a database
-
-  """
+  """Object to manage the connection to a database"""
 
   def __init__(self, fname):
     super(BibDb, self).__init__()
     self.fname = fname
-    con = create_connection(db_file)
+    con = create_connection(fname)
     if con is not None:
       self.con = con
       self.cur = con.cursor()
 
-    fields = get_dbcolnames(self.cur)
-    if fields ==
+      self.fields = get_dbcolnames(self.cur)
+      # if fields ==
 
 
 def opendb(db_file):
+  """
+
+  Parameters
+  ----------
+  db_file :
+
+
+  Returns
+  -------
+
+  """
   con = create_connection(db_file)
   return con
 
 
 def create_connection(db_file):
-  """ create a database connection to the SQLite database
+  """create a database connection to the SQLite database
       specified by the db_file
-  :param db_file: database file
-  :return: Connection object or None
+
+  Parameters
+  ----------
+  db_file :
+      database file
+
+  Returns
+  -------
+  type
+      Connection object or None
+
   """
   conn = None
   try:
@@ -39,25 +57,84 @@ def create_connection(db_file):
   return conn
 
 
-def get_dbcolnames(cur):
+def get_dbtablename(con):
+  """Get first table name form connected database
+
+  Parameters
+  ----------
+  con : sqlite3 connection
+
+
+  Returns
+  -------
+  string: table name
+  """
+  cur = con.cursor()
+  cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
+  tbns = cur.fetchone()
+  if tbns is None:
+    return ''
+  else:
+    return tbns[0]
+
+
+def get_dbcolnames(con):
+  """
+  Read column names from first table of connected database
+
+  Parameters
+  ----------
+  con : sqlite3 connection
+
+
+  Returns
+  -------
+  list: names of colunns (fields)
+
+  Notes
+  -----
+  Assume that only one table is present, or uses the first
+  """
+  tabnm = get_tablename(con)  # Get table name
+
+  cur = con.cursor()
   cols = []
-  for row in cur.execute(f"SELECT name FROM PRAGMA_TABLE_INFO('new_pubs2');"):
+  for row in cur.execute(f"SELECT name FROM PRAGMA_TABLE_INFO('{tabnm}');"):
     cols.append(row[0])
   return cols
 
 
-dbname = '../sqlite/articlesAll0c1delB.db'
+def create_bibtable(conn, fields, tablename='biblio'):
+  """Create a table to store the biblist bibliography
+
+  Parameters
+  ----------
+  conn : sqlite3 Connection object
+
+  fields : list
+      Columns to use
+  """
+
+  strcols = f"id integer PRIMARY KEY, \n  "
+  strcols += ",\n  ".join([f"{f.replace('-','_')} text" for f in fields])
+  try:
+    print(f"CREATE TABLE IF NOT EXISTS {tablename} (\n  {strcols}\n);")
+    c = conn.cursor()
+    c.execute(f"CREATE TABLE IF NOT EXISTS {tablename} (\n  {strcols}\n);")
+  except sq.Error as e:
+    print(e)
+
+
+dbname = '../sqlite/articlesAll0c1del.db'
+dbname = '../sqlite/new1.db'
 
 con = create_connection(dbname)
+create_bibtable(con, allfields)
+con.close()
 
-cur = con.cursor()
 
-# res = cur.execute("SELECT name FROM sqlite_master")
-# t = res.fetchall()
-# print(t)
-t = "new_pubs2"
-fields = []
-for row in cur.execute(f"SELECT name FROM PRAGMA_TABLE_INFO('new_pubs2');"):
-  print()
-  fields.append(row[0])
-# fields = [c[0] for c in cols]
+# # t = "new_pubs2"
+# fields = []
+# for row in cur.execute(f"SELECT name FROM PRAGMA_TABLE_INFO('new_pubs2');"):
+#   print()
+#   fields.append(row[0])
